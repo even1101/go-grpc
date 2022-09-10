3cb9a6b42ac650c4f138dfe430b8a47b9935feb2
even.chang <even1101u@gmail.com>
2022-09-10 13:14:56 +0800

    grpc credentials

--- greet/client/main.go
+++ greet/client/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"go-grpc/greet/proto"
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr)
+	tls := false // change that to true if needed
+	opts := []grpc.DialOption{}
 
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificate: %v\n", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		creds := grpc.WithTransportCredentials(insecure.NewCredentials())
+		opts = append(opts, creds)
+	}
+
+	conn, err := grpc.Dial(addr, opts...)
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
 	}
 
 	defer conn.Close()
-	// c := proto.NewGreetServiceClient(conn)
+	c := proto.NewGreetServiceClient(conn)
 
-	// doGreet(c)
+	doGreet(c)
 }
